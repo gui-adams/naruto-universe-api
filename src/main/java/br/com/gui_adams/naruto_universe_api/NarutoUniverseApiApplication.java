0ef4e00c3c990f3184742e2af4c510848d512965
adams-sw <guilherme.adams@simpleway.tech>
2026-02-18 18:01:55 -0300

    init

---
+++ src/main/java/br/com/gui_adams/naruto_universe_api/NarutoUniverseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gui_adams.naruto_universe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoUniverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoUniverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gui_adams/naruto_universe_api/NarutoUniverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gui_adams.naruto_universe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoUniverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
