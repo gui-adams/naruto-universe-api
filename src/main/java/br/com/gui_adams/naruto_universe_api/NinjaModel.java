8a8992261b817eb15b417419b0832545e404ffa6
adams-sw <guilherme.adams@simpleway.tech>
2026-02-19 10:19:00 -0300

    JPA

---
+++ src/main/java/br/com/gui_adams/naruto_universe_api/NinjaModel.java
@@ -0,0 +1,47 @@
+package br.com.gui_adams.naruto_universe_api;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro_de_ninjas")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public NinjaModel() {
+    }
+
+
+}
