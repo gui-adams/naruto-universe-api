3148448fcc662a367e55080f08f753fb4f0f264f
adams-sw <guilherme.adams@simpleway.tech>
2026-02-19 09:50:55 -0300

    Criado controller

---
+++ src/main/java/br/com/gui_adams/naruto_universe_api/Controller.java
@@ -0,0 +1,15 @@
+package br.com.gui_adams.naruto_universe_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/helloword")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa api";
+    }
+
+}
